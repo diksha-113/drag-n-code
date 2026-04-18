07cae7a69b657e5700eece60d2d15574c2394789
diksha-113 <147328332+diksha-113@users.noreply.github.com>
2026-04-18 13:58:47 +0530

    Update secrets.dart

--- screens/dashboard/secrets.dart
+++ screens/dashboard/secrets.dart
@@ -3,5 +3,5 @@
 // ⚠️ Keep this file out of public repos!
 // Add it to .gitignore if using Git.
 
-const String googleApiKey = "AIzaSyDaay2A8Fml5RUzi-GRiFj_pWDZNY6JEks";
-const String googleCx = "96e01a28cb4c34dcd";
+const String googleApiKey = "YOUR_API_KEY";
+const String googleCx = "YOUR_CX_KEY";
